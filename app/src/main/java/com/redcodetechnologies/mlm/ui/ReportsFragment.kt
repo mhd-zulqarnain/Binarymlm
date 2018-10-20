56d889ee098be0a6d6683f5257a512b91b8d263f
Zulqarnain <zuluparvi@gmail.com>
2018-10-20 18:50:41 +0500

    fragment

---
+++ app/src/main/java/com/redcodetechnologies/mlm/ui/ReportsFragment.kt
@@ -0,0 +1,18 @@
+package com.redcodetechnologies.mlm.ui
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.redcodetechnologies.mlm.R
+
+class ReportsFragment :  Fragment(){
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        var view = inflater.inflate(R.layout.fragment_reports, container, false)
+        // initView(view)
+        return view
+    }
+
+}
