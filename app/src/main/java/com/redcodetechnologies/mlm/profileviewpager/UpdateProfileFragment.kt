774423d34146212e99ee0b75302e6fd1903cacd8
abdulshakoor07 <abdulshakoordp1@gmail.com>
2018-10-24 18:54:12 +0500

    profile validiation

--- app/src/main/java/com/redcodetechnologies/mlm/profileviewpager/UpdateProfileFragment.kt
+++ app/src/main/java/com/redcodetechnologies/mlm/profileviewpager/UpdateProfileFragment.kt
@@ -8,10 +8,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.TextView
+import android.widget.*
 import com.redcodetechnologies.mlm.R
 import kotlinx.android.synthetic.main.activity_sign_in.*
 import kotlinx.android.synthetic.main.fragment_first.*
@@ -27,42 +24,48 @@ private const val ARG_PARAM2 = "param2"
  *
  */
 class FirstFragment : Fragment() {
-
+    var updateprofile : Button? = null
+    var name : EditText? = null
+    var username : EditText? = null
+    var address : EditText? = null
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         val view = inflater.inflate(R.layout.fragment_first, container, false)
 
-        val updateprofile = view!!.findViewById<Button>(R.id.btn_updateprofile)
-        val name = view!!.findViewById<EditText>(R.id.ed_name)
-        val username = view!!.findViewById<EditText>(R.id.ed_username)
-        val address = view!!.findViewById<EditText>(R.id.ed_address)
-        val country = view!!.findViewById<EditText>(R.id.ed_country)
+        updateprofile = view!!.findViewById<Button>(R.id.btn_updateprofile)
+        name = view!!.findViewById<EditText>(R.id.ed_name)
+        username = view!!.findViewById<EditText>(R.id.ed_username)
+        address = view!!.findViewById<EditText>(R.id.ed_address)
 
+        updateprofile!!.setOnClickListener(View.OnClickListener {
 
-        updateprofile.setOnClickListener(View.OnClickListener {
-
-            if(name.text.toString().trim(' ').length < 1) {
-                name.error = Html.fromHtml("<font color='#E0796C'>Name could not be empty</font>")
-                name.requestFocus()
-            }
-            else if(username.text.toString().trim(' ').length < 1){
-                username.error = Html.fromHtml("<font color='#E0796C'>User name could not be empty</font>")
-                username.requestFocus()
-            }
-            else if(address.text.toString().trim(' ').length < 1){
-                address.error = Html.fromHtml("<font color='#E0796C'>Address could not be empty</font>")
-                address.requestFocus()
-            }
-            else if(country.text.toString().trim(' ').length < 1){
-                country.error = Html.fromHtml("<font color='#E0796C'>Country could not be empty</font>")
-                country.requestFocus()
-            }
+            validiation()
 
         })
 
         return view
+    }
+    fun validiation(){
+
+        if(name!!.text.toString().trim(' ').length < 1) {
+            name!!.error = Html.fromHtml("<font color='#E0796C'>Name could not be empty</font>")
+            name!!.requestFocus()
+        }
+        else if(username!!.text.toString().trim(' ').length < 1){
+            username!!.error = Html.fromHtml("<font color='#E0796C'>User name could not be empty</font>")
+            username!!.requestFocus()
+        }
+        else if(address!!.text.toString().trim(' ').length < 1){
+            address!!.error = Html.fromHtml("<font color='#E0796C'>Address could not be empty</font>")
+            address!!.requestFocus()
+        }
+
+        else{
+            Toast.makeText(activity!!, "Profile has been Updated!" , Toast.LENGTH_LONG).show()
+        }
+
     }
 
 }
