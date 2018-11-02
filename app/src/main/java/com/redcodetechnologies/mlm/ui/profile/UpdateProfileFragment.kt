257a66435c982d005ff7aa24743b43480b3f6117
abdulshakoor07 <abdulshakoordp1@gmail.com>
2018-11-02 16:15:32 +0500

    dashboard work

--- app/src/main/java/com/redcodetechnologies/mlm/ui/profile/UpdateProfileFragment.kt
+++ app/src/main/java/com/redcodetechnologies/mlm/ui/profile/UpdateProfileFragment.kt
@@ -9,6 +9,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.*
 import com.redcodetechnologies.mlm.R
+import com.toptoche.searchablespinnerlibrary.SearchableSpinner
+import kotlinx.android.synthetic.main.report_dialog.view.*
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -25,7 +27,7 @@ class FirstFragment : Fragment() {
     var name : EditText? = null
     var username : EditText? = null
     var address : EditText? = null
-    var spinner_country : Spinner? = null
+    var spinner_country : SearchableSpinner? = null
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -40,6 +42,7 @@ class FirstFragment : Fragment() {
 
         initView()
         updateprofile!!.setOnClickListener{
+
             validiation()
         }
 
@@ -47,11 +50,27 @@ class FirstFragment : Fragment() {
     }
 
     private fun initView() {
+
         var arrayAdapter  =ArrayAdapter.createFromResource(activity!!,R.array.country_arrays,R.layout.support_simple_spinner_dropdown_item)
         spinner_country!!.adapter = arrayAdapter
+        spinner_country!!.setTitle("Select Country");
+        spinner_country!!.setPositiveButton("Close");
 
+        spinner_country!!.setSelection(166)
 
+            spinner_country!!?.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+
+                }
+
+                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                        Toast.makeText(activity!!, ""+(spinner_country!!.selectedItemPosition+1), Toast.LENGTH_SHORT).show()
+                }
+
+            }
     }
+
+
 
     fun validiation(){
 
